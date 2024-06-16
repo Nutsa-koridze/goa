61b0e2a79e4ba3f6082dd4b8b210d96469ee396b
Nutsa_koridze <Nutsaqoridze2gimail.com>
2024-06-16 15:27:00 +0400

    second commit

---
+++ level10/classwork/classwork.py
@@ -0,0 +1,17 @@
+# task 1
+for i in range(50, 100):
+    print(i)
+ 
+
+# task 2
+
+for i in range(10, 0, -1):
+    print(i)
+
+
+# task 3
+
+hello="Hello world"
+
+for hello in range(100):
+    print(hello)
